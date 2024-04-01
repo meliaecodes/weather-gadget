27a6d6386bd1b15efd93dce769c8b4bb1b8a5703
Melissa Paisley <mpaisley@atlassian.com>
2024-04-02 09:15:43 +1100

    added 5 day forecast

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,20 +1,140 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import ForgeReconciler, { Box, Heading, Inline, Image, Strong, Text, Form, FormHeader, FormSection, FormFooter, Label, xcss, RequiredAsterisk, Textfield, Button, useForm, useProductContext } from '@forge/react';
+import { invoke, requestJira, view } from '@forge/bridge';
 
+const containerStyles = xcss({
+  padding: 'space.200'
+});
 
-const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+export const Edit = () => {
+  // this screen will show when the gadget is in edit mode. 
+  const { handleSubmit, register, getFieldId } = useForm();
+
+  const configureGadget = (data) => {
+    view.submit(data);
+  };
 
   return (
     <>
-      <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+    <Form onSubmit={handleSubmit(configureGadget)}>
+      <FormHeader title="Login">
+        Required fields are marked with an asterisk <RequiredAsterisk />
+      </FormHeader>
+      <FormSection>
+      <Label labelFor="city">Your City<RequiredAsterisk /></Label>
+      <Textfield {...register("city")} name="city" placeholder="Sydney"/>
+      <Label labelFor="country">Your Country<RequiredAsterisk /></Label>
+      <Textfield {...register("country")} name="country" placeholder="Australia"/>
+      </FormSection>
+      <FormFooter>
+        <Button appearance="primary" type="submit">
+          Submit
+        </Button>
+      </FormFooter>
+    </Form>
     </>
   );
+};
+
+const View = () => {
+  // this will display when the gadget is in view mode. 
+  const [coords, setCoords] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [fiveDayForecast, setFiveDayForecast] = useState(null);
+  
+  const context = useProductContext();
+
+  useEffect(() => {
+    console.log("view: context change detected");
+    console.log(context);
+    if(context){
+      setLocation(context.extension.gadgetConfiguration);
+    }
+  }, [context]);
+
+  useEffect(() => {
+    console.log("view: location change detected")
+    console.log(location);
+    if(location!==null){
+      invoke('getLocationCoordinates', {location: location}).then(setCoords);
+    }
+  }, [location]);
+
+  useEffect(() => {
+    console.log("view: coordinate change detected")
+    console.log(coords);
+    invoke('getWeather', {coords: coords}).then(setWeather);
+    invoke('getForecast', {coords: coords}).then(setForecast);
+  }, [coords]);
+
+  useEffect(() => {
+    console.log("view: forecast change detected")
+    console.log(forecast);
+    invoke('getFiveDayForecast', {forecast: forecast}).then(setFiveDayForecast);
+  }, [forecast]);
+
+  useEffect(() => {
+    console.log("view: weather change detected")
+    console.log(weather);
+  }, [weather]);
+
+  useEffect(() => {
+    console.log("view: 5 day forecast change detected")
+    console.log(fiveDayForecast);
+  }, [fiveDayForecast]);
+
+  return (
+    <Box xcss={containerStyles}>
+      <Heading as="h2">The Weather</Heading>
+      <Text><Strong>Location:</Strong> {location ? (`${location.city}, ${location.country}`) : `Please edit the macro to enter your location`}</Text>
+      <Inline>
+        <Box xcss={containerStyles}>
+          <Image src={weather ? (`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`) : "https://openweathermap.org/img/wn/01d@2x.png"} alt={weather ? weather.weather[0].description : "Loading"} />
+        </Box>
+        <Box xcss={containerStyles}>
+          <Text><Strong>Current Temperature</Strong> {weather ? weather.main.temp : '[ ]'} °C</Text>
+          <Text><Strong>Feels like:</Strong> {weather ? weather.main.feels_like : '[ ]'} °C</Text>
+          <Text><Strong>Humidity:</Strong> {weather ? weather.main.humidity : '[ ]'}%</Text>
+        </Box >
+      </Inline>
+      <Text>Forecast</Text>
+      <Inline>
+      {fiveDayForecast ? fiveDayForecast.map(dailyForecast => (
+      <Box xcss={containerStyles}>
+        <Image src={dailyForecast ? (`https://openweathermap.org/img/wn/${dailyForecast.icon}d@2x.png`) : "https://openweathermap.org/img/wn/01d@2x.png"} alt={dailyForecast ? dailyForecast.description : "Loading"} />
+        <Text><Strong>High:</Strong> {dailyForecast ? dailyForecast.maxTemp : '[ ]'} °C</Text>
+        <Text><Strong>Low:</Strong> {dailyForecast ? dailyForecast.minTemp : '[ ]'} °C</Text>
+        <Text><Strong>Humidity:</Strong> {dailyForecast ? Math.round(dailyForecast.humidity.sum/dailyForecast.humidity.count) : '[ ]'}%</Text>
+        <Text><Strong>Rain:</Strong> {dailyForecast ? Math.round(dailyForecast.totalRain) : '[ ]'}mm</Text>
+      </Box>
+      )) : <Text>Loading...</Text>}
+      </Inline>
+    </Box>
+  )
+};
+
+const App = () => {
+  // this determines whether the app is in view mode or edit mode
+
+  const context = useProductContext();
+  console.log("App: context")
+  console.log(context);
+
+
+  if (!context) {
+    console.log('App: no context')
+    return (
+      <>
+        <Text>This never actually appears</Text>
+        <Text>Because Jira displays a default screen</Text>
+      </>
+    );
+  }
+
+  return context.extension.entryPoint === "edit" ? <Edit /> : <View />;
+
 };
 
 ForgeReconciler.render(
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,4 +1,6 @@
 import Resolver from '@forge/resolver';
+import { fetch } from '@forge/api'
+
 
 const resolver = new Resolver();
 
@@ -7,5 +9,188 @@ resolver.define('getText', (req) => {
   console.log(req);
   return 'Hello, world!';
 });
+
+resolver.define('getLocationCoordinates', async (req) => {
+  console.log('location payload')
+  console.log(req.payload);
+  if(req.payload.location != null){
+    
+    const url = "https://api.openweathermap.org/geo/1.0/direct?q=" + req.payload.location.city + "," + req.payload.location.country + "&limit=5&appid=" + process.env.OPENWEATHER_KEY;
+    const response = await fetch(url)
+    if(!response.ok) {
+      const errmsg = `Error from Open Weather Map Geolocation API: ${response.status} ${await response.text()}`;
+      console.error(errmsg)
+      throw new Error(errmsg)
+    }
+    const locations = await response.json()
+
+    console.log('location json')
+    console.log(locations[0]);
+    console.log(locations);
+    console.log('location response')
+    console.log(response);
+
+    return locations[0];
+  } else {
+    return null;
+  }
+});
+
+resolver.define('getFiveDayForecast', async (req) => {
+  console.log("FiveDayForecast")
+  console.log(req.payload.forecast);
+
+if(req.payload.forecast != null){
+  const tz = req.payload.forecast.city.timezone;
+
+  // determine the first second (adjusted locally) for each of the 5 days
+  // and create the 5 day forcast array
+  let firstDate = new Date((req.payload.forecast.list[0].dt)*1000);
+  let startFirstDate = new Date()
+  startFirstDate.setUTCFullYear(firstDate.getUTCFullYear());
+  startFirstDate.setUTCMonth(firstDate.getUTCMonth());
+  startFirstDate.setUTCDate(firstDate.getUTCDate());
+  startFirstDate.setUTCHours(0);
+  startFirstDate.setUTCMinutes(0);
+  startFirstDate.setUTCSeconds(0);
+  startFirstDate.setUTCMilliseconds(0);
+
+  const localStart = ((startFirstDate.valueOf()/1000) - tz);
+  let localStartDate = new Date(localStart*1000);
+  const dailyHighLow = [];
+  const day = 60 * 60 * 24;
+  for (let i = 0; i < 6; i++) {
+    dailyHighLow[i] = {
+      minTemp: null,
+      maxTemp: null,
+      totalRain: 0, 
+      humidity: {sum: 0, count: 0},
+      icon: '01',
+      description: 'clear sky'
+    }
+  }
+  // for each list item
+// work out which local day it applies to
+// check if new low temp
+// check if new high temp
+  console.log("DailyHighLow Array Initialised")
+  console.log(dailyHighLow);
+
+  req.payload.forecast.list.forEach((data) => {
+    const ds = data.dt - localStart;
+    
+    let n = Math.floor(ds/day);
+
+    // the array will start at -1 for UTC- timezones so we need to adjust
+    if(tz < 0) { 
+      n++;
+    }
+    
+    console.log("n = " + n)
+    console.log("ds = " + ds)
+    
+
+    if (n < 6) {
+
+      if(dailyHighLow[n].minTemp == null) {
+        dailyHighLow[n].minTemp = data.main.temp;
+      } else {
+        if (dailyHighLow[n].minTemp > data.main.temp) {
+          dailyHighLow[n].minTemp = data.main.temp;
+        }
+      }
+      if(dailyHighLow[n].maxTemp == null) {
+        dailyHighLow[n].maxTemp = data.main.temp;
+      } else {
+        if (dailyHighLow[n].maxTemp < data.main.temp) {
+          dailyHighLow[n].maxTemp = data.main.temp;
+        }
+      }
+      
+      // storing the total rain. 
+      if(data.rain){
+        dailyHighLow[n].totalRain = dailyHighLow[n].totalRain + data.rain['3h']
+      }
+
+      // storing values to calculate the humidity average
+      dailyHighLow[n].humidity.sum = data.main.humidity + dailyHighLow[n].humidity.sum;
+      dailyHighLow[n].humidity.count++;
+
+      // determining if the weather conditions are 'worse' based on their icon number. 
+      // more information about weather icons at https://openweathermap.org/weather-conditions
+      if(dailyHighLow[n].icon < (data.weather[0].icon.substr(0,2)))
+      {
+        console.log(dailyHighLow[n].icon + " < " + data.weather[0].icon.substr(0,2));
+        dailyHighLow[n].icon = data.weather[0].icon.substr(0,2);
+        dailyHighLow[n].description = data.weather[0].description;
+      }
+    }
+
+  });
+
+  console.log(dailyHighLow);
+  // just return the data for the days after today
+  return dailyHighLow.slice(1);
+}
+return null;
+});
+
+resolver.define('getForecast', async (req) => {
+  if(req.payload.coords != null){
+
+    const lon = req.payload.coords.lon;
+    const lat = req.payload.coords.lat;
+    const url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + process.env.OPENWEATHER_KEY;
+    const response = await fetch(url)
+    if(!response.ok) {
+      const errmsg = `Error from Open Weather Map Weather API: ${response.status} ${await response.text()}`;
+      console.error(errmsg)
+      throw new Error(errmsg)
+    }
+  
+    const weather = await response.json()
+  
+    console.log('getForecast response')
+    console.log(response);
+    console.log('getForcast json')
+    console.log(weather);
+  
+    return weather;
+    }
+    else 
+    {
+      return null;
+    }
+})
+
+resolver.define('getWeather', async (req) => {
+
+  if(req.payload.coords != null){
+
+  const lon = req.payload.coords.lon;
+  const lat = req.payload.coords.lat;
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + process.env.OPENWEATHER_KEY;
+  const response = await fetch(url)
+  if(!response.ok) {
+    const errmsg = `Error from Open Weather Map Weather API: ${response.status} ${await response.text()}`;
+    console.error(errmsg)
+    throw new Error(errmsg)
+  }
+
+  const weather = await response.json()
+
+  console.log('getweather response')
+  console.log(response);
+  console.log('getweather json')
+  console.log(weather);
+
+  return weather;
+  }
+  else 
+  {
+    return null;
+  }
+
+})
 
 export const handler = resolver.getDefinitions();
